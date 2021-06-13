e3de8088c8eca486ebcc121680291df9bf075817
pomyslowynick <rafal.rosa@mycit.ie>
2021-06-13 23:15:57 +0100

    Add http client stub

---
+++ proglog/server/http.go
@@ -0,0 +1,41 @@
+package server
+
+import "net/http"
+
+func NewHTTPServer(addr string) *http.Server {
+	httpsrv := NewHTTPServer()
+	r := mux.NewRouter()
+
+	r.HandleFunc("/", httpsrv.handleProduce).Methods("POST")
+	r.HandleFunc("/", httpsrv.handleConsume).Methods("GET")
+	return &http.Server(
+		Addr: addr,
+		Handler: r,
+	)
+}
+
+type httpServer struct {
+	Log *Log
+}
+
+func newHTTPServer() *httpServer {
+	return &httpServer{
+		Log: NewLog()
+	}
+}
+
+type ProduceRequest struct {
+	Record Record `json:"record"`
+}
+
+type ProduceResponse struct {
+	Offset uint64 `json:"offset"`
+}
+
+type ConsumeRequest struct {
+	Offset uint64 `json:"offset"`
+}
+
+type ConsumeResponse struct {
+	Record Record `json:"record"`
+}
